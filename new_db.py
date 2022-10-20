0422508b5630b6832fff62630b41081164bac373
Vi <380992566619v@gmail.com>
2022-10-20 17:33:51 +0300

    start_crating_db

--- new_db.py
+++ new_db.py
@@ -1,9 +1,11 @@
 from peewee import *
 from baseModels import ShowUser, AllUsers
 from playhouse.shortcuts import model_to_dict
+from playhouse.postgres_ext import JSONField
 import logging
 from dotenv import load_dotenv
 import os
+from datetime import datetime
 logging.basicConfig(level=logging.INFO)
 load_dotenv()
 db = PostgresqlDatabase('game',
@@ -14,12 +16,27 @@ db = PostgresqlDatabase('game',
 
 
 class MyUser(Model):
-    name = CharField(unique=True)
+    email = CharField(unique=True)
+    login = CharField(unique=True)
     password = CharField()
     balance = IntegerField()
+    best_score = IntegerField(null=True)
+    created_date = DateField(default=datetime.now().strftime("%Y-%m-%d"))
+    NFT = None
 
     class Meta:
         database = db
+
+
+class Statistics(Model):
+    price_enter = FloatField()
+    money_to_winner = FloatField()
+    winner = ForeignKeyField(MyUser, backref='winning')
+    ended = DateTimeField(default=datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+
+class NFT(Model):
+    name = CharField()
 
 
 def show_all() -> AllUsers:
