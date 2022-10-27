634bf7e29946c83f7435c60b69bf48b7fc349528
Vi <380992566619v@gmail.com>
2022-10-27 11:21:15 +0300

    added_aouth2

--- src/web_app/models/admin_model.py
+++ src/web_app/models/admin_model.py
@@ -1,7 +1,7 @@
 from peewee import Model, CharField
 from src.utils.database.connect_to_db import db
 
-
+# maybe will be in the future
 class Admin(Model):
     username = CharField(unique=True)
     password = CharField()
